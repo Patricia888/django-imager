cd5d0306ac0113fd8f2a10b340a60c92e239af0a
George Ceja <net.george.sky@gmail.com>
2018-04-23 17:13:28 -0700

    Masked Testing

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import ImagerProfile, User
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ImagerProfile
+
+    phone = factory.Faker('phone_number')
+    location = factory.Faker('street_address')
+    website = factory.Faker('uri')
+    fee = 45
+    camera = 'dsr'
+
+
+class ProfileUnitTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestCase, cls)
+        for _ in range(50):
+            user = UserFactory.create()
+            user.set_password(factory.Faker('password'))
+            user.save()
+
+            profile = ProfileFactory.create(user=user)
+            profile.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestCase, cls)
+        User.objects.all().delete()
+
+    def test_user_can_see_its_profile(self):
+        one_user = User.objects.first()
+        self.assertIsNotNone(one_user.profile)
