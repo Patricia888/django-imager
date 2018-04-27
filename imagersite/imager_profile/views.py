60051aeef28f11fb2eae628aa7456a721feaabef
George Ceja <net.george.sky@gmail.com>
2018-04-26 20:00:17 -0700

    Added Photo and Albums for users personal storage

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -29,5 +29,83 @@ def profile_view(request, username=None):
     return render(request, "imager_profile/profile.html", context)
 
 
-def settings_view(request):
+def settings_view(request, username=None):
     pass
+
+
+def library_view(request, username=None):
+    owner = False
+
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == "":
+            return redirect("home")
+
+    profile = get_object_or_404(ImagerProfile, user__username=username)
+    albums = Albums.objects.filter(user__username=username)
+    photos = Photo.objects.filter(product__user__username=username)
+
+    if not owner:
+        photos = Photo.objects.filter(published="PUBLIC")
+        albums = Albums.objects.filter(published="PUBLIC")
+
+    context = {
+        "profile": profile,
+        "albums": albums,
+        "photos": photos
+    }
+
+    return render(request, "imager_profile/library.html", context)
+
+
+def albums_view(request, username=None):
+    owner = False
+
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == "":
+            return redirect("home")
+
+    profile = get_object_or_404(ImagerProfile, user__username=username)
+    albums = Albums.objects.filter(user__username=username)
+    photos = Photo.objects.filter(product__user__username=username)
+
+    if not owner:
+        photos = Photo.objects.filter(published="PUBLIC")
+        albums = Albums.objects.filter(published="PUBLIC")
+
+    context = {
+        "profile": profile,
+        "albums": albums,
+        "photos": photos
+    }
+
+    return render(request, "imager_profile/albums.html", context)
+
+
+def photo_view(request, username=None):
+    owner = False
+
+    if not username:
+        username = request.user.get_username()
+        owner = True
+        if username == "":
+            return redirect("home")
+
+    profile = get_object_or_404(ImagerProfile, user__username=username)
+    albums = Albums.objects.filter(user__username=username)
+    photos = Photo.objects.filter(product__user__username=username)
+
+    if not owner:
+        photos = Photo.objects.filter(published="PUBLIC")
+        albums = Albums.objects.filter(published="PUBLIC")
+
+    context = {
+        "profile": profile,
+        "albums": albums,
+        "photos": photos
+    }
+
+    return render(request, "imager_profile/photo.html", context)
