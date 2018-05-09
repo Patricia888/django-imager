3caf48ed22e23dbbdb087656244e245fc747ab52
Patricia G Raftery <praftery8@gmail.com>
2018-05-09 16:09:48 -0700

    profile class view working heyyyy, albums and photos count working heyyyy

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import profile_view, settings_view
+from .views import ProfileView, settings_view
 
 urlpatterns = [
-    path("", profile_view, name="profile"),
-    path("<str:username>/", profile_view, name="named_profile"),
+    path("", ProfileView.as_view(), name="profile"),
+    path("<str:username>/", ProfileView.as_view(), name="named_profile"),
     path("settings/<str:username>", settings_view, name="settings"),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from imager_images.models import Albums, Photo
 from .models import ImagerProfile
+from django.views.generic import DetailView
+from django.conf import settings
 
 
-def profile_view(request, username=None):
-    '''Shows profile.'''
-    owner = False
+# def profile_view(request, username=None):
+#     '''Shows profile.'''
+#     owner = False
 
-    if not username:
-        username = request.user.get_username()
-        owner = True
-        if username == "":
-            return redirect("home")
+#     if not username:
+#         username = request.user.get_username()
+#         owner = True
+#         if username == "":
+#             return redirect("home")
 
-    profile = get_object_or_404(ImagerProfile, user__username=username)
-    albums = Albums.objects.filter(user__username=username)
-    photos = Photo.objects.filter(albums__user__username=username)
+#     profile = get_object_or_404(ImagerProfile, user__username=username)
+#     albums = Albums.objects.filter(user__username=username)
+#     photos = Photo.objects.filter(albums__user__username=username)
 
-    if not owner:
-        photos = Photo.objects.filter(published="PUBLIC")
-        albums = Albums.objects.filter(published="PUBLIC")
+#     if not owner:
+#         photos = Photo.objects.filter(published="PUBLIC")
+#         albums = Albums.objects.filter(published="PUBLIC")
 
-    context = {
-        "profile": profile,
-        "albums": albums,
-        "photos": photos,
-    }
+#     context = {
+#         "profile": profile,
+#         "albums": albums,
+#         "photos": photos,
+#     }
 
-    return render(request, "imager_profile/profile.html", context)
+#     return render(request, "imager_profile/profile.html", context)
 
 
 def settings_view(request, username=None):
@@ -35,3 +37,48 @@ def settings_view(request, username=None):
     pass
 
 
+# profile class
+class ProfileView(DetailView):
+    template_name = 'imager_profile/profile.html'
+    slug_url_kwarg = 'username'
+    slug_field = 'user__username'
+    model = ImagerProfile
+    context_object_name = 'profile'
+
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('home')
+        # import pdb; pdb.set_trace()
+        if kwargs:
+            return super().get(*args, **kwargs)
+
+        else:
+            self.kwargs.update({'username': self.request.user.username})
+            # kwargs.update({'owner': True})
+
+        return super().get(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # import pdb; pdb.set_trace()
+
+        # profile = get_object_or_404(ImagerProfile, user__username=kwargs['username'])
+        albums = Albums.objects.filter(user__username=self.request.user.username)
+        photos = Photo.objects.filter(user__username=self.request.user.username)
+        # number_of_photos = photos.count()
+        # number_of_albums = albums.count()
+
+        if context['profile'].user.username != self.request.user.username:
+            photos = photos.filter(published='PUBLIC')
+            albums = albums.filter(published='PUBLIC')
+
+        # context['profile'] = profile
+        context['pub_albums'] = albums
+        context['pub_photos'] = photos
+        context['priv_albums'] = albums.filter(published='PRIVATE')
+        context['priv_photos'] = photos.filter(published='PRIVATE')
+        # context['number_of_photos'] = number_of_photos
+        # context['number_of_albums'] = number_of_albums
+
+        return context
